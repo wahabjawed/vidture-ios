a4629a506b6cb644533b1a9ead8c425820a71a26
Wahab Jawed <wahabjawed@gmail.com>
2014-08-06 11:48:30 +0500

    Initial Commit

---
+++ vidture/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  vidture
+//
+//  Created by Zainu Corporation on 06/08/2014.
+//  Copyright (c) 2014 Zainu Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
