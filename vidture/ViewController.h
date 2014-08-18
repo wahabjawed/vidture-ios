1f97330c76489cad56b045ddffa6b99d9a5d42b5
Zain memon <zainumemon@gmail.com>
2014-08-18 13:40:37 +0500

    Commit

--- vidture/ViewController.h
+++ vidture/ViewController.h
@@ -1,13 +1,24 @@
 //
 //  ViewController.h
-//  vidture
+//  NMPopUpView
 //
-//  Created by Zainu Corporation on 06/08/2014.
-//  Copyright (c) 2014 Zainu Corporation. All rights reserved.
+//  Created by Nikos Maounis on 9/12/13.
+//  Copyright (c) 2013 Nikos Maounis. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "PopUpViewController.h"
+#import "secondPopUp.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController : UIViewController
+
++ (BOOL)myBool;
++ (void)setMyBool:(BOOL)value;
++ (void)getMyBool:(BOOL)value;
+
+@property (weak, nonatomic) IBOutlet UIButton *showPopupBtn;
+@property (strong, nonatomic) PopUpViewController *popViewController;
+@property (strong, nonatomic) secondPopUp *secondPopUp;
 
 @end
